c70307c6bd31b62b6b768ce7048550cf48b17c13
Eugeny Shaposhnikov <eawapownikov@gmail.com>
2024-04-29 23:29:23 +0300

    nvim all settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 
 -----------------------------------------------------------
--- Common
+-- ОБЩИЕ ОПЦИИ
 -----------------------------------------------------------
 opt.mouse = 'a'              --Включит мышь
 opt.encoding = 'utf-8'       --Кодировка
@@ -13,7 +13,7 @@ syntax enable
 opt.swapfile = false         --Не создаем свап файлы
 
 -----------------------------------------------------------
--- Visual
+-- ВИЗУАЛЬНЫЕ ОПЦИИ
 -----------------------------------------------------------
 opt.number = true            --Номер строк сбоку
 opt.wrap = true              --Длинные линии будет видно
@@ -29,7 +29,7 @@ autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml,htmljinja setlocal 
 
 opt.so = 5                   --Отступ курсора от края экрана
 opt.foldcolumn = '2'         --Ширина колонки для фолдов
-opt.colorcolumn =  '80'      --Расположение цветной колонки
+opt.colorcolumn =  '119'     --Расположение цветной колонки
 
 -- remove line lenght marker for selected filetypes
 vim.cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]]
@@ -59,4 +59,59 @@ autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", ti
 augroup end
 ]], false)
 
+-----------------------------------------------------------
+-- НАСТРОЙКИ ПЛАГИНОВ
+-----------------------------------------------------------
+-- LSP settings
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+    if server.name == "sumneko_lua" then
+        -- only apply these settings for the "sumneko_lua" server
+        opts.settings = {
+            Lua = {
+                diagnostics = {
+                    -- Get the language server to recognize the 'vim', 'use' global
+                    globals = {'vim', 'use'},
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
+        }
+    end
+    server:setup(opts)
+end)
 
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+vim.o.completeopt = 'menuone,noselect'
+-- luasnip setup
+local luasnip = require 'luasnip'
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+        { name = 'buffer', option = {
+            get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+            end
+          },
+        },
+   },
+}
